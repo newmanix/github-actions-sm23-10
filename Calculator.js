ef6ebda282296f65c605a5ab86533bdd49ffec05
Bill Newman <william.newman@seattlecolleges.edu>
2023-08-17 11:05:47 -0700

    Update Calculator.js, adds multiply()
    Still needs subtract.

--- Calculator.js
+++ Calculator.js
@@ -24,7 +24,9 @@ class Calculator{
 	   return num1 / num2;
   }
 
-}
+  multiply(num1=0,num2=0){	
+   return num1 * num2;
+ }
 
 //required so data is visible externally
 module.exports = Calculator;
